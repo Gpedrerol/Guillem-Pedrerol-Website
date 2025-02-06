483233c62707cb0268eee5c76ddaf6b8c08d98c3
guillempedrerol <guillem.pedrerol@gmail.com>
2025-02-06 13:37:53 +0100

    Fix: Update i18n server configuration

--- lib/i18n/server.ts
+++ lib/i18n/server.ts
@@ -1,6 +1,6 @@
 import { createI18nServer } from 'next-international/server'
 
-export const { getI18n, getScopedI18n, I18nProviderServer } = createI18nServer({
+export const { getI18n, getScopedI18n } = createI18nServer({
   en: () => import('../../messages/en.json'),
   es: () => import('../../messages/es.json')
 })
